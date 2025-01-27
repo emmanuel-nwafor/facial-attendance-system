19b6f732067676cdf7841fe310de08615eb5876d
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-01-27 04:23:25 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <img
+        src="https://cdn.prod.website-files.com/6364b6fd26e298b11fb9391f/64816006b11ff55138c6962a_Thumbnail-00135.png"
+        alt=""
+      />
+      <h1 className="text-3xl font-bold">Hello Tailwind css v4</h1>
+    </div>
+  );
+}
+
+export default App;
