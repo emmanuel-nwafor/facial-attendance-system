d75cbc05c496745171e4714522b51f316b893834
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-01-27 19:26:23 -0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Welcome from "./pages/Welcome";
+import Signup from "./pages/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <img
-        src="https://cdn.prod.website-files.com/6364b6fd26e298b11fb9391f/64816006b11ff55138c6962a_Thumbnail-00135.png"
-        alt=""
-      />
-      <h1 className="text-3xl font-bold">Hello Tailwind css v4</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Signup() {
+  return (
+    <>
+      <div className="bg-gradient-to-r from-blue-50 to-blue-100 min-h-screen flex flex-col justify-center items-center">
+        <h1>Hello Signup</h1>
+      </div>
+    </>
+  );
+}
