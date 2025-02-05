522fafa705e752587e2c34044dcfbda277e97724
emmanuel nwafor <emitexc.e.o1@gmail.com>
2025-02-05 02:38:22 -0800

    Resolved merge conflict and added Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -15,4 +6,3 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
->>>>>>> bc64162c9c4bb2b553e9d211c0e1a1b7ee7ab9a5
